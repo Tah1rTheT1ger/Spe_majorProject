23274f01dad473155a68f0d81eaf6fcba79a157b
Tah1rTheT1ger <tahirkhadarabad@gmail.com>
2025-12-11 15:40:29 +0530

    last but one

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -33,7 +33,7 @@ export default function Register() {
         <select value={form.role} onChange={e => setForm({ ...form, role: e.target.value })}>
           <option value="patient">Patient</option>
           <option value="doctor">Doctor</option>
-          <option value="desk">Desk beanch this time one change okay last change </option>
+          <option value="desk">Desk beanch this time one change okay last but one </option>
         </select>
         <button type="submit">Register</button>
         <p style={{ marginTop: 10 }}>
